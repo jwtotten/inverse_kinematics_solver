97f77aaed1fb610d8371b97bb37e6fb6345fd3ee
Jack <jacktotten@hotmail.co.uk>
2025-04-11 22:05:07 +0100

    Adding .gitignore and initial inverse kinematics solver script.

---
+++ iksolver.py
@@ -0,0 +1,7 @@
+# This file is for solving the inverse kinematics of a hexapod robot.
+
+import numpy as np
+from math import pi, sin, cos, atan2, sqrt
+
+if __name__ == "__main__":
+    print('Inverse Kinematics Solver for Hexapod Robot')
