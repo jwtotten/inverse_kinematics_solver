50b0e6ed7f74b8ac26cf5bf25d7672f4bb75b029
Jack <jacktotten@hotmail.co.uk>
2025-08-22 08:14:05 +0100

    set up of iksolver class for tests implemented.

--- Unittests/ik_solver_tests.py
+++ Unittests/ik_solver_tests.py
@@ -1,12 +1,15 @@
 import unittest
 from Scripts.iksolver import IkSolver
 
+FEMUR_LENGTH = 3.5
+TIBIA_LENGTH = 3.5
+
 class IkSolverTests(unittest.TestCase):
     def setUp(self):
         """
         Set Up for the IK solver class.
         """
-        self.ik_solver = IkSolver()
+        self.ik_solver = IkSolver(femur_length=FEMUR_LENGTH, tibia_length=TIBIA_LENGTH)
 
     def test_ik_solver(self):
         # Your test code here
